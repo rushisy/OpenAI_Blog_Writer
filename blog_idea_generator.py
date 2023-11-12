1177b5276033ab65a0aad323fe0b9c7d486566a7
Rushi Yalamanchili <rushisy@gmail.com>
2023-11-12 01:02:18 -0600

    openai integration

--- blog_idea_generator.py
+++ blog_idea_generator.py
@@ -1,6 +1,7 @@
 import requests #used to download raw xml data from napkintheory's feed
 import xml.etree.ElementTree as ET #used to iterate the raw xml file
 from lxml.html import fromstring #used to clean-up the html formatting, so i am left with clean text
+from openai import OpenAI
 
 
 # download rss feed from substack
@@ -27,17 +28,39 @@ titles = []
 description = []
 content = []
 
+titlesText = ""
+descriptionText = ""
+contentText = ""
+i = 1
+
 # iterate the child and search for 'item' identifiers -- they contain the actual article data
 
 for item in root.iter('item'):
-    titles.append(fromstring(item[0].text).text_content()) #title
-    description.append(fromstring(item[1].text).text_content()) #description
-    content.append(fromstring(item[7].text).text_content()) #content
+    title = fromstring(item[0].text).text_content()
+    subtitle = fromstring(item[1].text).text_content()
+    article = fromstring(item[7].text).text_content()
 
-# note data is already clean by this point, using lxml.html api (live saver)
+    titles.append(title) #title
+    titlesText += "title " + str(i) + ": " + title + "\n"
+    
+    description.append(subtitle) #description
+    descriptionText += "description " + str(i) + ": " + subtitle + "\n"
+    
+    content.append(article) #content
+    contentText += "content " + str(i) + ": " + article + "\n"
 
-print(titles)
-print(description)
-print(content)
+    i+=1
 
-#integrate w openai somehow
+
+# note data is already clean by this point, using lxml.html api (live saver), integrating with openai api
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a blog idea generator based on the content the user will submit. Provide some topics they could write about, but not the of ANY topics the input given is related too."},
+        {"role": "user", "content": "educate me my boi" + titlesText + descriptionText + "remember no topics about what is given to you, think what other NON RELATED topics they could enjoy writing about"}
+    ]
+)
+print(response.choices[0].message)
